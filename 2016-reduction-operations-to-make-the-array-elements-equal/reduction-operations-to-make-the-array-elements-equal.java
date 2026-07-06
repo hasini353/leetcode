d69388590c16f059d4186a4ff7dae643c2e5c657
Hasini Gundubogula <156277912+hasini353@users.noreply.github.com>
2026-07-06 22:07:38 +0530

    Time: 39 ms (31.12%) | Memory: 71.3 MB (84.86%) - LeetSync

---
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reductionOperations(int[] nums) {
+        Arrays.sort(nums);
+       int op=0;
+       int cnt=0;
+       for(int i=1;i<nums.length;i++){
+         if(nums[i]!=nums[i-1]){
+            cnt++;
+            } 
+            op+=cnt;
+       }
+       return op;
+    }
+}
